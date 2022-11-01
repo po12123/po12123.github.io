3de8f9e0f01ca03f5141aa9787a1b30f53a353e4
po12123 <58833511+po12123@users.noreply.github.com>
2022-11-01 01:12:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-
 <?php 
-$dbServerName  =  "ejemplo.com" ; 
-$dbUsername  =  "exdbuser" ; 
-$dbPassword  =  "exdbpass" ; 
-$dbName  =  "codexworld" ; 
+$servidor = "us-cdbr-east-06.cleardb.net:3306";
+$usuario = "b37e4c4c79ae20";
+$password = "fe44c49f";
+$bd = "heroku_d49cd007ce5f8e7";
 
 // crear conexión 
-$conn  = new  mysqli ( $dbServerName ,  $dbUsername ,  $dbPassword ,  $dbName ); 
-
+$conn  = new  mysqli ( $servidor ,  $usuario ,  $password ,  $bd ); 
+echo  "antes de verificar" ; 
 // comprueba la conexión 
-si ( $conn -> connect_error ) { 
+if ( $conn -> connect_error ) { 
 	echo  "no se conecto" ; 
     die( "Error en la conexión: "  .  $conn -> connect_error );
 } 
